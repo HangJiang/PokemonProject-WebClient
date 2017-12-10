fe1a06bae36767b2d45b81a84df1f9a67021004c
HangJiang <hitjh2012@163.com>
2017-12-10 23:14:29 +0800

    update

--- src/mockdata/pokemons.js
+++ src/mockdata/pokemons.js
@@ -1,12 +1,10 @@
 export const Pokemons = [
     {
-        pmid: 1,
+        id: 1,
         dexNo: "1",
-        nameCN: "妙蛙种子"
+        nameCN: "妙蛙种子",
+        baseStat: {hp: 100, atk: 100, def:100,spatk:100,spdef:100, spd:100},
+        ability:{"0":"111","1":"222","3":"asd"}
     },
-    {
-        pmid: 2,
-        dexNo: "2",
-        nameCN: "妙蛙草"
-    },
+
 ]
